17ac4c0ab0f2fc415aaa738c848ad58d793d4346
raphaeldeletoille <58251038+raphaeldeletoille@users.noreply.github.com>
2023-11-22 13:13:15 +0100

    add secret

--- main.tf
+++ main.tf
@@ -45,3 +45,47 @@ resource "azurerm_storage_container" "container" {
 # terraform fmt 
 
 #DEPLOYER UN KEYVAULT SUR VOTRE RG
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "kv" {
+  name                        = "raphkv"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id # = 68a63944-3750-446d-855d-3691acf10ab8
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Set",
+      "Get",
+      "Delete",
+      "Purge",
+      "Recover"
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
+
+#DEPLOYER UN SECRET DANS VOTRE KEYVAULT 
+resource "azurerm_key_vault_secret" "mdp" {
+  name         = "mdpdatabase"
+  value        = "qIQHFh87-!$$"
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+#GENEREZ UN MOT DE PASSE ALEATOIRE POUR REMPLACER LA VALUE DE VOTRE SECRET 
