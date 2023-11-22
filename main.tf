7f6b9ef4dab8877361b03a7efb83668ff28c7d4c
raphaeldeletoille <58251038+raphaeldeletoille@users.noreply.github.com>
2023-11-22 11:02:32 +0100

    add storage °+kv exercice

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.81.0"
     }
   }
@@ -18,3 +18,30 @@ resource "azurerm_resource_group" "rg" {
 
 #DEPLOYER UN STORAGE ACCOUNT DANS VOTRE RESOURCE GROUP
 #VOTRE STORAGE ACCOUNT DOIT ETRE EN "COOL"
+
+resource "azurerm_storage_account" "sto" {
+  name                     = "raphstorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  access_tier              = "Cool"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "vhds"
+  storage_account_name  = azurerm_storage_account.sto.name
+  container_access_type = "private"
+}
+
+# https://github.com/raphaeldeletoille/fdme
+
+# terraform init
+# terraform plan
+# terraform apply 
+# terraform apply -auto-approve
+# terraform validate
+# terraform destroy
+# terraform fmt 
+
+#DEPLOYER UN KEYVAULT SUR VOTRE RG
