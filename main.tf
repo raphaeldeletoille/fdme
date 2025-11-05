7165dbb20dd84c22b24b82b70e4a59ba451a4cee
raphaeldeletoille <raphael.deletoille@gmail.com>
2025-11-05 15:43:08 +0100

    add Sas

--- main.tf
+++ main.tf
@@ -38,3 +38,44 @@ resource "azurerm_storage_container" "container" {
 
 #CREER UN SAS TOKEN ET VOUS CONNECTER A VOTRE CONTAINER (Terraform)
 #DEPUIS AZURE STORAGE EXPLORER ET DEPOSER UN FICHIER TEXTE (Azure Storage Explorer)
+data "azurerm_storage_account_sas" "sas" {
+    connection_string = azurerm_storage_account.storage.primary_connection_string
+    https_only           = true
+
+    resource_types {
+        service   = true
+        container = true
+        object    = true
+    }
+
+    services {
+        blob  = true
+        queue = true
+        table = true
+        file  = true
+    }
+
+    start  = "2025-03-21T00:00:00Z"
+    expiry = "2026-03-21T00:00:00Z"
+
+    permissions {
+        read    = true
+        write   = true
+        delete  = true
+        list    = true
+        add     = true
+        create  = true
+        update  = true
+        process = true
+        tag     = true
+        filter  = true
+    }
+}
+
+
+output "container_sas_url" {
+    value       = nonsensitive("${azurerm_storage_account.storage.primary_blob_endpoint}${data.azurerm_storage_account_sas.sas.sas}")
+    sensitive   = false
+}
+
+#DEPLOYER UN KEYVAULT EN MODE ACCESS POLICY, DE VOUS DONNER (A VOUS MEME) tous les droits "Secret" et créer un Secret.
